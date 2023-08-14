eed8b12ec8bb21b9f6d088ab4d7e7d74c59d5737
Michael <michaelkmx2@gmail.com>
2023-08-14 19:39:43 -0300

    Inicialização do Repositório

---
+++ api-michael/src/main/java/br/com/apimichael/ApiMichaelApplication.java
@@ -0,0 +1,13 @@
+package br.com.apimichael;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMichaelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMichaelApplication.class, args);
+	}
+
+}
---
+++ api-michael/src/test/java/br/com/apimichael/ApiMichaelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.apimichael;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMichaelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
