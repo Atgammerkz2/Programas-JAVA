a60cbbe406e1944a751e22dfed99f53a4f515f8e
Michael <michaelkmx2@gmail.com>
2023-08-16 07:54:04 -0300

    CRUD básico, faltando apenas função DELETE

--- api-michael/src/main/java/br/com/apimichael/controller/ProdutoController.java
+++ api-michael/src/main/java/br/com/apimichael/controller/ProdutoController.java
@@ -1,28 +1,35 @@
 package br.com.apimichael.controller;
 import br.com.apimichael.entities.Produto;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class ProdutoController {
-
-    private  static Produto[] produtos = new Produto[]{
-            new Produto(1l, "Coca-cola", 10.0f),
-            new Produto(2l, "Extra Power 1250ml", 12.0f),
-    };
+    private static List<Produto> produtos = new ArrayList<>();
+    private static final DecimalFormat df = new DecimalFormat("0.00");
 
     @GetMapping(path = "/produtos", produces = "application/json")
-    public Produto[] getProdutos(){
+    public List<Produto> getProdutos(){
         return produtos;
     }
 
     @GetMapping(path = "/produtos/{produtoId}")
     public String findById(@PathVariable Long produtoId){
-        if(produtoId == 1){
-            return "Produto 1";
+        var produto = produtos.stream().filter(x -> x.getId().equals(produtoId)).findFirst().orElse(null);
+
+        if(produto != null){
+            return String.format("ID: %d</br>Nome: %s</br>Preço: R$ %s", produto.getId(),
+                    produto.getNome(), df.format(produto.getPreco()));
         }
 
         return "Produto não encontrado!";
+    }
+
+    @PostMapping(path = "/produtos/add")
+    public void addProduto(@RequestBody Produto produto){
+        produtos.add(produto);
     }
 }
